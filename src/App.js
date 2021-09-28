2dc81d4822d2e7cef2d5b92954261b2e78782827
Ayush Kumar <kumarayush7767@gmail.com>
2021-09-28 19:14:24 +0530

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import React,{ useState , useEffect} from 'react';
 import './App.css';
 
 function App() {
+
+  const getTheme = () =>{
+    //parse means convert string into javascript object
+    return JSON.parse(localStorage.getItem("theme")) || false
+  }
+
+  const [theme,setTheme]=useState(getTheme());
+  
+  useEffect(() => {
+    //To Convert a JavaScript object into a string with JSON.stringify().
+      localStorage.setItem("theme" , JSON.stringify(theme))
+  },[theme]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={theme ? "theme-dark": "" }>
+      <div className="content-bg-color main-content">
+       <h1>Dark Mode is </h1>
+       <input type="checkbox" onChange={ () => setTheme(!theme) } checked={theme ? "true" : ""} />
+      </div>
     </div>
   );
 }
